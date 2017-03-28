a9a82c5ee0fc512daca15afa24e849f5093a0cb2
Juan Carlos Medina <juanca.med@gmail.com>
2017-03-28 15:45:10 -0700

    Remove bad example for source code examples; rename legends

--- src/all-examples.jsx
+++ src/all-examples.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import PresentationalTextField from './presentational-text-field.jsx';
-import PresentationalMoneyTextField from './presentational-money-text-field.jsx';
 import CompositionalMoneyTextField from './compositional-money-text-field.jsx';;
 
 import RememberValueHigherOrderComponent from './higher-order-component-remember-value.jsx';
@@ -19,32 +18,27 @@ export default class AllExamples extends React.Component {
     return (
       <div>
         <fieldset>
-          <legend>[Presentational] Text Field</legend>
+          <legend>[Presentational Component] Text Field</legend>
           <PresentationalTextField onChange={() => console.log('Type ')} />
         </fieldset>
 
         <fieldset>
-          <legend>[Copypasta] Money Text Field</legend>
-          <PresentationalMoneyTextField onChange={() => console.log('Type $$$$ ')} />
-        </fieldset>
-
-        <fieldset>
-          <legend>[Presentational Higher Order Component] Money Text Field</legend>
+          <legend>[Compositional Component] Money Text Field</legend>
           <CompositionalMoneyTextField onChange={() => console.log('HOC $$$$')} />
         </fieldset>
 
         <fieldset>
-          <legend>[Behavioral Higher Order Component] Stateful Text Field</legend>
+          <legend>[Higher-order Component] Stateful Text Field</legend>
           <RememberedTextField />
         </fieldset>
 
         <fieldset>
-          <legend>[Behavioral Higher Order Component] Stateful Money Text Field</legend>
+          <legend>[Higher-order Component] Stateful Money Text Field</legend>
           <RememberedMoneyTextField />
         </fieldset>
 
         <fieldset>
-          <legend>[Container] Stateful Text Field</legend>
+          <legend>[Connected Component] Stateful Text Field</legend>
           <ContainerTextField />
         </fieldset>
       </div>
